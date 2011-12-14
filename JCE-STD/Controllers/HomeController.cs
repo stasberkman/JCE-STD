90496b612ee6c8d20c51d731b9df768ef95db5fa
arielle <ariellvy@gmail.com>
2011-12-14 13:00:59 +0200

    JCE-STD

---
+++ JCE-STD/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JCE_STD.Controllers
+{
+    public class HomeController : Controller
+    {
+        //HOME/INDEX
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to JCE-STD!";
+
+            return View();
+        }
+        //HOME/ABOUT
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
